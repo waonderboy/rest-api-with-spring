cb25f0f7bf27d5c2220a7ec25bc9781d5ffbd3d7
waonderboy <crazy7523@gmail.com>
2022-09-23 17:12:45 +0900

    Event Controller

---
+++ src/main/java/com/example/restapi/events/EventController.java
@@ -0,0 +1,29 @@
+package com.example.restapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        // ResponseEntity를 사용하는 이유:: 응답 코드, 헤더, 본문 모두 다루기 편한 API
+        // ResponseEntity.created() uri location 필요
+        // HATEOS가 제공하는 linkTo(), methodOn() 사용
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
---
+++ src/test/java/com/example/restapi/events/EventControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.restapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ExtendWith(SpringExtension.class)
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with spring")
+                .beginEnrollDateTime(LocalDateTime.of(2022, 11, 23, 14 ,21))
+                .closeEnrollDateTime(LocalDateTime.of(2022, 11, 23, 14 ,21))
+                .beginEventDateTime(LocalDateTime.of(2022, 11, 23, 14 ,21))
+                .endEventDateTime(LocalDateTime.of(2022, 11, 23, 14 ,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .LimitOfEnrollment(100)
+                .location("강남역 Naver D2 Factory")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+
+    }
+
+}
